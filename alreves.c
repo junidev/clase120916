19166f32168279711bb019dbf76655808a7395c6
junidev <junigomez1305@gmail.com>
2016-09-13 09:46:00 -0500

    Update alreves.c

--- alreves.c
+++ alreves.c
@@ -6,15 +6,15 @@ main() {
     printf("Introduzca la secuencia de entrada: ");
     scanf("%d",cadena);
     printf("\nLa secuencia invertida es: ");
-    invertir(cadena)|;
+    invertir(cadena);
 }
 
-void invertirCadena(char cad[], int pos) {
-static int i = 0;
-char tmp;
-if (cad[pos])
-invertirCadena(cad, pos + 1);
-tmp = cad[pos];
-cad[pos] = cad[i];
-cad[i++] = tmp;
+void invertir(char cad[], int pos) {
+    static int i = 0;
+    char tmp;
+    if (cad[pos])
+        invertir(cad, pos + 1);
+        tmp = cad[pos];
+        cad[pos] = cad[i];
+        cad[i++] = tmp;
 }
