ccc446cd91ebd9c4e4fa4ef54afbd35a9381346e
junidev <junigomez1305@gmail.com>
2016-09-13 09:34:53 -0500

    Update buscar.c

--- buscar.c
+++ buscar.c
@@ -1,18 +1,20 @@
-#include <string.h>
 #include <stdio.h>
 
-main()
-{
-char caract, cadena[80], *ptr;
-printf("\nIntroduzca la cadena donde se va a buscar:\n");
-//para leer cadenas de texto con espacios usamos gets
-gets(cadena);
-printf("\nEscriba el caracter a buscar:\n");
-//para leer un solo caracer podemos usar getchar
-caract=getchar();
-//para buscar una cadena dentro de otra usamos strstr(contenida y nos arroja la posicion)
-ptr=strchr(cadena, caract);
-if (ptr==0) printf("\nEl caracter %c no se encuentra en la cadena.\n",caract);
-else printf("La posicion del caracter es %d.\n", ptr-cadena+1);
-getch();
+int strindex(char *cadena, int caracter){
+    int i = 0;
+    pos = -1;
+    while (*cadena++ != '\0'){
+        ++i;
+        if (*cadena == caracter)
+            pos = i;
+    }
+    return pos;
+}
+
+main(){
+    char *cadena="Ciencia de la computacion";
+    char caracter;
+    printf("Escriba caracter a buscar")
+    scanf("%c",&caracter);
+    printf("%d",strindex(cadena,caracter));
 }
